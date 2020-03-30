6080c8f793649b8033a365d0050a5455fe69b8e0
ehgud0670 <kimdo2297@gmail.com>
2020-03-30 19:34:59 +0900

    [style] 관측소 이름은 station으로도 충분이 설명되어 클래스이름을 변경함

--- iOS/DustNoticeApp/DustNoticeApp/StationViewController.swift
+++ iOS/DustNoticeApp/DustNoticeApp/StationViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class MeasuringStationViewController: UIViewController {
+final class StationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
