8ba8c6cc9b0e9ce5cc10ffdef53076aa068fcfad
ehgud0670 <kimdo2297@gmail.com>
2020-03-30 16:24:06 +0900

    [style] 각 뷰 컨트롤러 역할에 맞게 이름 변경함         MeasuringStationViewController : 관측소 기준 미세먼지 현황을 보여주는 뷰 컨트롤러         MapViewController : 지도 기준 미세먼지 예보를 보여주는 뷰 컨트롤러 [docs] swift 용 .gitignore 문서 추가함

---
+++ iOS/DustNoticeApp/DustNoticeApp/MeasuringStationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DustNoticeApp
+//
+//  Created by kimdo2297 on 2020/03/30.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class MeasuringStationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
