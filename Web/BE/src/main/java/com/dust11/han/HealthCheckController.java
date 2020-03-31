0291c5f08ee0febfe0076524d31772b2e391890c
Han <quaint3@gmail.com>
2020-03-31 14:34:53 +0900

    [feat] swagger cofing
    스웨거 확인할 수 있도록 설정
    API 결과를 반환할 객체 생성
    헬스체크 컨트롤러 수정

--- Web/BE/src/main/java/com/dust11/han/HealthCheckController.java
+++ Web/BE/src/main/java/com/dust11/han/HealthCheckController.java
@@ -1,15 +1,21 @@
 package com.dust11.han;
 
+import com.dust11.han.api.ApiResult;
+import io.swagger.annotations.Api;
+import java.text.SimpleDateFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/_hcheck")
+@Api(tags = "헬스체크 API")
 public class HealthCheckController {
 
+  private final static SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
   @GetMapping
-  public Long healthCheck() {
-    return System.currentTimeMillis();
+  public ApiResult<String> healthCheck() {
+    return ApiResult.OK(format1.format(System.currentTimeMillis()));
   }
 }
