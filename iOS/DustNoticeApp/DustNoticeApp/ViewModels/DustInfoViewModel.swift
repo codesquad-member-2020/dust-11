e4237b3a4d7236d589207357194b4b3b3a296d2f
ehgud0670 <kimdo2297@gmail.com>
2020-04-01 13:01:33 +0900

    [refactor] 1.DustInfo 셀의 상태바의 width의 constant를 pm10Value로 두었는데 수치가 작은것같아 pm10Value x 2 로 줌. 	   2. 관련 속성을 private 으로 두고 대신 외부에서 사용할 수 있는 함수를 만듬

--- iOS/DustNoticeApp/DustNoticeApp/ViewModels/DustInfoViewModel.swift
+++ iOS/DustNoticeApp/DustNoticeApp/ViewModels/DustInfoViewModel.swift
@@ -19,8 +19,7 @@ final class DustInfoViewModel {
     func configure(_ dustInfoCell: DustInfoCell) {
         dustInfoCell.dustValueLabel.text = dustValueString
         dustInfoCell.dustValueBar.backgroundColor = backgroundColor
-        guard let barWidthConstraintLayout = dustInfoCell.barWidthConstraintLayout else { return }
-        barWidthConstraintLayout.constant = CGFloat(dustValue)
+        dustInfoCell.setBarWitdhConstraint(constant: CGFloat(dustValue))
     }
     
     var dustValue: UInt {
