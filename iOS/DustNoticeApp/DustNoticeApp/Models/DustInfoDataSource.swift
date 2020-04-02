d2fd3f8fc7a63d755fbb51b3a3b62b14a9d0e22c
ehgud0670 <kimdo2297@gmail.com>
2020-04-02 20:52:10 +0900

    [refactor] 로드한 데이터를 DustInfoDataSource의 DustInfoViewModels에 주입하기 위해 코드 수정함 	   기존에 있었던 dummyData를 지움

--- iOS/DustNoticeApp/DustNoticeApp/Models/DustInfoDataSource.swift
+++ iOS/DustNoticeApp/DustNoticeApp/Models/DustInfoDataSource.swift
@@ -8,66 +8,17 @@
 
 import UIKit
 
-enum DummyData {
-    enum DummyDust {
-        static let good = DustInfo(dataTime: "10", pm10Grade1h: "1", pm10Value: "0")
-        static let usual = DustInfo(dataTime: "50", pm10Grade1h: "2", pm10Value: "0")
-        static let bad = DustInfo(dataTime: "120", pm10Grade1h: "3", pm10Value: "0")
-        static let veryBad = DustInfo(dataTime: "200", pm10Grade1h: "4", pm10Value: "0")
-    }
-    
-    enum DummyDustViewModels {
-        static let goods: [DustInfoViewModel] = {
-            var viewModels = [DustInfoViewModel]()
-            for _ in 0 ..< DustInfoDataSource.Quantity.numberOfRows / 4 {
-                viewModels.append(DustInfoViewModel(dustInfo: DummyDust.good))
-            }
-            return viewModels
-        }()
-        static let usuals: [DustInfoViewModel] = {
-            var viewModels = [DustInfoViewModel]()
-            for _ in 0 ..< DustInfoDataSource.Quantity.numberOfRows / 4 {
-                viewModels.append(DustInfoViewModel(dustInfo: DummyDust.usual))
-            }
-            return viewModels
-        }()
-        static let bads: [DustInfoViewModel] = {
-            var viewModels = [DustInfoViewModel]()
-            for _ in 0 ..< DustInfoDataSource.Quantity.numberOfRows / 4 {
-                viewModels.append(DustInfoViewModel(dustInfo: DummyDust.bad))
-            }
-            return viewModels
-        }()
-        static let veryBads: [DustInfoViewModel] = {
-            var viewModels = [DustInfoViewModel]()
-            for _ in 0 ..< DustInfoDataSource.Quantity.numberOfRows / 4 {
-                viewModels.append(DustInfoViewModel(dustInfo: DummyDust.veryBad))
-            }
-            return viewModels
-        }()
-        
-        static let max: [DustInfoViewModel] = {
-            var viewModels = [DustInfoViewModel]()
-            viewModels.append(contentsOf: goods)
-            viewModels.append(contentsOf: usuals)
-            viewModels.append(contentsOf: bads)
-            viewModels.append(contentsOf: veryBads)
-            return viewModels
-        }()
-    }
-}
-
 final class DustInfoDataSource: NSObject, UITableViewDataSource {
-    let dustInfoViewModels = DustInfoViewModels(dustInfoViewModels: DummyData.DummyDustViewModels.max)
+    let dustInfoViewModels: DustInfoViewModels
+    
+    init(dustInfoViewModels: DustInfoViewModels) {
+        self.dustInfoViewModels = dustInfoViewModels
+    }
 }
 
 extension DustInfoDataSource {
-    enum Quantity {
-        static let numberOfRows = 100
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Quantity.numberOfRows
+        return dustInfoViewModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- iOS/DustNoticeApp/DustNoticeApp/StationViewController.swift
+++ iOS/DustNoticeApp/DustNoticeApp/StationViewController.swift
@@ -14,7 +14,7 @@ final class StationViewController: UIViewController {
     @IBOutlet weak var statusView: StatusView!
     
     //MARK:- Internal Property
-    private let dustInfoDataSource = DustInfoDataSource()
+    private var dustInfoDataSource: DustInfoDataSource!
     private let dustInfoDelegate = DustInfoDelegate()
     private let locationManager = LocationManager()
     override func viewDidLoad() {
@@ -38,6 +38,8 @@ final class StationViewController: UIViewController {
             guard let station = station else { return }
             DispatchQueue.main.async {
                 self.configureStatusView(station.stationName)
+                self.configureDataSource(dustInfos: station.list)
+                self.dustInfoTableView.reloadData()
             }
         }
     }
@@ -46,12 +48,17 @@ final class StationViewController: UIViewController {
         statusView.stationLabel.text = "\(stationName) 측정소 기준"
     }
     
+    private func configureDataSource(dustInfos: [DustInfo]) {
+        let dustInfoViewModels = dustInfos.map{ DustInfoViewModel(dustInfo: $0) }
+        dustInfoDataSource = DustInfoDataSource(dustInfoViewModels: DustInfoViewModels(dustInfoViewModels))
+        dustInfoTableView.dataSource = dustInfoDataSource
+    }
+    
     private func configureDustInfoDelegate() {
         dustInfoDelegate.statusView = statusView
     }
     
     private func configureDustInfoTableView() {
-        dustInfoTableView.dataSource = dustInfoDataSource
         dustInfoTableView.delegate = dustInfoDelegate
         dustInfoTableView.register(DustInfoCell.self,
                                    forCellReuseIdentifier: DustInfoCell.reuseIdentifier)
