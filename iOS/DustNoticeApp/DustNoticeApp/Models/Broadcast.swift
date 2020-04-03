1ef9c914472b9c3f1d55b550b1b3ddf60f57876e
ehgud0670 <kimdo2297@gmail.com>
2020-04-03 11:25:38 +0900

    [feat] 예보 데이터를 처리하기 위한 Codable 객체들 생성함

---
+++ iOS/DustNoticeApp/DustNoticeApp/Models/Broadcast.swift
@@ -0,0 +1,25 @@
+//
+//  Broadcast.swift
+//  DustNoticeApp
+//
+//  Created by kimdo2297 on 2020/04/03.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import Foundation
+
+struct Broadcast: Codable {
+    let list: [DustInform]
+}
+
+struct DustInform: Codable {
+    let imageUrl1: String
+    let imageUrl2: String
+    let imageUrl3: String
+    let imageUrl4: String
+    let imageUrl5: String
+    let imageUrl6: String
+    var imageUrl7: String?
+    let informGrade: String
+    let informOverall: String
+}
